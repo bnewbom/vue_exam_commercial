e49608c5ad620b5ed86ef0c72fe28b6b4e3fe7fa
Spring <bnewbom@gmail.com>
2022-02-05 15:00:47 +0900

    데이터 유효성 검증

--- pages/index.vue
+++ pages/index.vue
@@ -66,6 +66,7 @@
         v-model="cardNum"
         :rules="cardRules"
         label="신용카드"
+        :counter="19"
         required
       ></v-text-field>
       <v-btn
@@ -85,6 +86,7 @@
   </div>
 </template>
 
+<script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
 <script>
 export default {
   name: 'IndexPage',
@@ -94,16 +96,137 @@ export default {
       stepTwoValid: false,
       stepThreeValid: false,
       usrName: '',
-      nameRules: [],
+      nameRules: [
+        v => !!v || '필수 입력 필드입니다.',
+        v => this.validName(v) || '이름 형식이 맞지 않습니다. (한글 또는 영문 2자 이상)',
+      ],
       phone:'',
-      phoneRules: [],
+      phoneRules: [
+        v => !!v || '필수 입력 필드입니다.',
+        v => this.validPhone(v) || '핸드폰 번호 형식이 맞지 않습니다.',
+      ],
       email: '',
-      emailRules: [],
+      emailRules: [
+        v => !!v || '필수 입력 필드입니다.',
+        v => this.validEmail(v) || '이메일 형식이 맞지 않습니다.',
+      ],
       cardNum:'',
-      cardRules:[]
+      cardRules:[
+        v => !!v || '필수 입력 필드입니다.',
+        v => this.validCard(v) || '신용카드 형식이 맞지 않습니다.',
+      ]
     }
   },
   methods:{
+    validate () {
+      console.log(this.$refs.form)
+        
+    },
+
+    validName(v){
+      return /^[가-힣a-zA-Z]{2}$/.test(v)
+    },
+
+    validPhone(v){
+      return /^01([0|1|6|7|8|9])[- ]?([0-9]{3,4})[- ]?([0-9]{4})$/.test(v)
+    },
+
+    validEmail(v){
+      return /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i.test(v)
+    },
+
+    validCard(v){
+      if(/^([0-9]{4})[- ]?([0-9]{4})[- ]?([0-9]{4})$/.test(v)){
+        this.validCardNum(v)
+      }else{
+        return false
+      }
+    },
+
+    validCardNum(v){
+      //카드 번호 유효성 체크
+      //뒤에서부터 홀수번째 자리의 숫자를 모두 더한 값을 홀수합
+      //뒤에서부터 짝수번째 자리의 숫자에 2을 곱하되, 만일 2를 곱한 수가 두자리수 숫자일 경우 두 수를 더 한 값을, 한자 리수라면 그 숫자 그대로를 모두 더한 값이 짝수합
+      //홀수합과 짝수합을 더한 값이 10의 배수이면 유효
+      v = v.replace(/\s|-/gi,'').split('').reverse()
+
+      let odd = v.filter((el,index)=>{
+        return index % 2 == 0
+      })
+      let oddSum = 0
+      odd.forEach(el=>{oddSum += Number(el)})
+
+      let even = v.filter((el,index)=>{
+        return index % 2 != 0
+      })
+      let evenSum = 0
+      even.forEach(el=>{
+        if(Number(el)*2 >= 10){
+          let temp = (Number(el)*2).toString().split('')
+          evenSum += Number(temp[0]) 
+          evenSum += Number(temp[1]) 
+        }else{
+          evenSum += Number(el)*2
+        }
+      })
+      return (oddSum + evenSum) % 10 == 0 ? true : false 
+    },
+
+
+    sample4_execDaumPostcode() {
+        new daum.Postcode({
+            oncomplete: function(data) {
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
+                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                var roadAddr = data.roadAddress; // 도로명 주소 변수
+                var extraRoadAddr = ''; // 참고 항목 변수
+
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraRoadAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                   extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if(extraRoadAddr !== ''){
+                    extraRoadAddr = ' (' + extraRoadAddr + ')';
+                }
+
+                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                document.getElementById('sample4_postcode').value = data.zonecode;
+                document.getElementById("sample4_roadAddress").value = roadAddr;
+                document.getElementById("sample4_jibunAddress").value = data.jibunAddress;
+                
+                // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
+                if(roadAddr !== ''){
+                    document.getElementById("sample4_extraAddress").value = extraRoadAddr;
+                } else {
+                    document.getElementById("sample4_extraAddress").value = '';
+                }
+
+                var guideTextBox = document.getElementById("guide");
+                // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
+                if(data.autoRoadAddress) {
+                    var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
+                    guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
+                    guideTextBox.style.display = 'block';
+
+                } else if(data.autoJibunAddress) {
+                    var expJibunAddr = data.autoJibunAddress;
+                    guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
+                    guideTextBox.style.display = 'block';
+                } else {
+                    guideTextBox.innerHTML = '';
+                    guideTextBox.style.display = 'none';
+                }
+            }
+        }).open();
+    }
   }
 }
 </script>
